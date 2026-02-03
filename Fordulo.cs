d1715110822bfdb1d0132f4910f88138a3a43456
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-02-03 09:16:07 +0100

    Add project files.

---
+++ Fordulo.cs
@@ -0,0 +1,30 @@
+﻿class Fordulo
+{
+    public int Ev { get; set; }
+    public int Het { get; set; }
+    public int Sorszam { get; set; }
+    public int Telitalalatok { get; set; }
+    public int Nyeremeny { get; set; }
+    public string Eredmenyek { get; set; }
+    public bool VoltTelitalalat => Telitalalatok > 0 || Nyeremeny > 0;
+
+    public bool NemVoltDontetlen => !Eredmenyek.ToUpper().Contains("X");
+
+    public override string ToString() =>
+        $"\tÉv: {Ev}\n" +
+        $"\tHét: {Het}.\n" +
+        $"\tForduló: {Sorszam}.\n" +
+        $"\tTelitalálat: {Telitalalatok} db\n" +
+        $"\tNyeremény: {Nyeremeny} Ft\n" +
+        $"\tEredmények: {Eredmenyek}";
+
+    public Fordulo(string[] v)
+    {
+        Ev = int.Parse(v[0]);
+        Het = int.Parse(v[1]);
+        Sorszam = int.Parse(v[2]);
+        Telitalalatok = int.Parse(v[3]);
+        Nyeremeny = int.Parse(v[4]);
+        Eredmenyek = v[5];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿const string SrcPath = "..\\..\\..\\res\\toto.txt";
+
+List<Fordulo> totoFordulok = [];
+
+using StreamReader sr = new(SrcPath);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+    totoFordulok.Add(new(sr.ReadLine().Split(';')));
+
+Console.WriteLine($"fordulok szama: {totoFordulok.Count}");
+
+var sumTT = totoFordulok.Sum(f => f.Telitalalatok);
+Console.WriteLine($"telitalalatos szelvenyek szama: {sumTT}");
+
+var avgKifizetes = totoFordulok
+    .Where(f => f.VoltTelitalalat)
+    .Average(f => f.Nyeremeny * f.Telitalalatok);
+Console.WriteLine($"kifizetesek atlaga: {avgKifizetes:0}");
+
+var maxNyeremenyF = totoFordulok
+    .MaxBy(f => f.Nyeremeny);
+Console.WriteLine($"legnagyobb nyeremeny:\n{maxNyeremenyF}");
+
+var minNyeremenyF = totoFordulok
+    .Where(f => f.VoltTelitalalat)
+    .MinBy(f => f.Nyeremeny);
+Console.WriteLine($"legkisebb nyeremeny:\n{minNyeremenyF}");
+
+var vhnvDontetlen = totoFordulok.Any(f => f.NemVoltDontetlen);
+Console.WriteLine($"{(vhnvDontetlen ? "volt" : "nem volt")} dontetlen nelkuli fordulo");
